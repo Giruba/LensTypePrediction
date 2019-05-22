771502a59c1baa627b998efef737586d4ecbf42e
Giruba Beulah SE <Giruba@users.noreply.github.com>
2019-05-23 03:01:05 +0530

    Update Program.cs

--- LensTypePredictions/Program.cs
+++ LensTypePredictions/Program.cs
@@ -32,7 +32,6 @@ namespace LensTypePredictions
             public float Label;
         }
 
-        // IrisPrediction is the result returned from prediction operations
         public class LensTypePrediction
         {
             [ColumnName("PredictedLabel")]
@@ -41,35 +40,15 @@ namespace LensTypePredictions
 
         static void Main(string[] args)
         {
-            // STEP 2: Create a pipeline and load your data
-            var pipeline = new LearningPipeline();
 
-            // If working in Visual Studio, make sure the 'Copy to Output Directory' 
-            // property of iris-data.txt is set to 'Copy always'
+            var pipeline = new LearningPipeline();
             string dataPath = Path.Combine(Environment.CurrentDirectory, "lenses.data.txt");
             pipeline.Add(new TextLoader(dataPath).CreateFrom<LensDataModel>(separator: ','));
-
-            // STEP 3: Transform your data
-            // Assign numeric values to text in the "Label" column, because only
-            // numbers can be processed during model training
             pipeline.Add(new Dictionarizer("Label"));
-
-            // Puts all features into a vector
             pipeline.Add(new ColumnConcatenator("Features", "AgeTypeOfPatient", "SpectaclePrescribed_Myop_Hyper", "PresenceOfAstigmatism", "TearProductionRate"));
-
-            // STEP 4: Add learner
-            // Add a learning algorithm to the pipeline. 
-            // This is a classification scenario (What type of iris is this?)
             pipeline.Add(new StochasticDualCoordinateAscentClassifier());
-
-            // Convert the Label back into original text (after converting to number in step 3)
             pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
-
-            // STEP 5: Train your model based on the data set
             var model = pipeline.Train<LensDataModel, LensTypePrediction>();
-
-            // STEP 6: Use your model to make a prediction
-            // You can change these numbers to test different predictions
             var prediction = model.Predict(new LensDataModel()
             {
                 AgeTypeOfPatient = 1,
